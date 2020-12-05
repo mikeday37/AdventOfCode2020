68e04cd4d86a33fbe347eb3aa45e051e4d93964f
Mike Day <mikeday370146@gmail.com>
2020-12-05 12:35:38 -0500

    switched wpf app to use .Net 5, and used C# 9's "top-level statements" in program.cs

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,17 +1,12 @@
-﻿using AdventOfCodeScaffolding;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// This is just to try C# 9's new "Top-level statements"
+// Would be a simple one-liner except for that fact that Program.Main needs the STAThread attribute.
+// See: https://stackoverflow.com/questions/64946371/how-to-handle-stathread-in-c-sharp-9-using-top-level-program-cs
 
-namespace AdventOfCode2020
-{
-	class Program
-	{
-        [STAThread]
-        public static void Main()
-        {
-            var app = new AdventOfCodeApp();
-            app.Run();
-        }
-	}
-}
+using System.Threading;
+using AdventOfCodeScaffolding;
+
+var t = new Thread(()=>
+	new AdventOfCodeApp().Run()
+);
+t.SetApartmentState(ApartmentState.STA);
+t.Start();
