70728914d8dd5314086bc66e9e15f0063958df30
mikeday37 <mikeday370146@gmail.com>
2020-12-16 17:25:44 -0500

    example using LogWindow in Day9

--- AdventOfCode2020/Challenges/Day9/Day9.cs
+++ AdventOfCode2020/Challenges/Day9/Day9.cs
@@ -6,6 +6,7 @@ using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2020.Challenges.Day9
@@ -55,14 +56,23 @@ namespace AdventOfCode2020.Challenges.Day9
 
 			// say goodbye to performance...
 			foreach (var start in Enumerable.Range(0, nums.Length))
+			{
+				using (Logger.Context($"start = {start}"))
 				foreach (var end in Enumerable.Range(start + 1, nums.Length - 1 - start))
 				{
+					if (end % 100 == 0)
+					{
+						Logger.LogLine($"end = {end}");
+						Thread.Sleep(250);
+					}
+
 					AllowCancel();
 
 					var suspect = nums[start..end];
 					if (suspect.Sum() == target)
 						return suspect.Min() + suspect.Max();
 				}
+			}
 
 			return -1;
 		}
