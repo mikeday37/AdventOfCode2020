2715731868aebbb53ff57c09540f93c4f40aea94
mikeday37 <mikeday370146@gmail.com>
2020-12-17 18:50:59 -0500

    day 17 solved

--- AdventOfCode2020/Challenges/Day17/Day17.cs
+++ AdventOfCode2020/Challenges/Day17/Day17.cs
@@ -15,203 +15,207 @@ namespace AdventOfCode2020.Challenges.Day17
 	[Challenge(17, "Conway Cubes")]
 	class Day17Challenge : ChallengeBase
 	{
-		public record InclusiveRange
+		public class PocketDimension
 		{
-			public int Min {get; init;}
-			public int Max {get; init;}
+			private readonly HashSet<(int, int, int)> live = new();
 
-			public InclusiveRange(int min, int max)
+			public int XMin { get; private set; }
+			public int XMax { get; private set; }
+			public int YMin { get; private set; }
+			public int YMax { get; private set; }
+			public int ZMin { get; private set; }
+			public int ZMax { get; private set; }
+
+			public void Add((int, int, int) c)
 			{
-				if (min <= max)
-					(Min, Max) = (min, max);
-				else
-					throw new Exception($"InclusiveRange must be created with min <= max.  Attempted min = {min}, max = {max}.");
+				if (c.Item1 < XMin) XMin = c.Item1;
+				if (c.Item1 > XMax) XMax = c.Item1;
+
+				if (c.Item2 < YMin) YMin = c.Item2;
+				if (c.Item2 > YMax) YMax = c.Item2;
+
+				if (c.Item3 < ZMin) ZMin = c.Item3;
+				if (c.Item3 > ZMax) ZMax = c.Item3;
+
+				live.Add(c);
 			}
 
-			public InclusiveRange((int, int) t) : this(t.Item1, t.Item2) {}
-
-			public int Size => 1 + Max - Min;
-
-			public bool IsValid(int i) => Min <= i && i <= Max;
-
-			public IEnumerable<int> YieldAll()
+			public bool Contains((int, int, int) c)
 			{
-				for (int i = Min; i <= Max; i++)
-					yield return i;
+				return live.Contains(c);
 			}
 
-			public IEnumerable<int> YieldInner()
+			public IEnumerable<string> ToDisplayStrings()
 			{
-				for (int i = Min + 1; i <= Max - 1; i++)
-					yield return i;
-			}
-		}
-
-		public class NDimensionalGrid<T>
-		{
-			public int NumDimensions { get; }
-			public IReadOnlyList<InclusiveRange> Extent { get; }
-			public int CellCount { get; }
-
-			private readonly T[] grid;
-
-			public T this[int i]
-			{
-				[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-				get { ValidateIndex(i); return grid[i]; }
-
-				[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-				set { ValidateIndex(i); grid[i] = value; }
-			}
-
-			public T this[IReadOnlyList<int> coordinates]
-			{
-				[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-				get { ValidateCoordinates(coordinates); return grid[ToIndex(coordinates)]; }
-
-				[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-				set { ValidateCoordinates(coordinates); grid[ToIndex(coordinates)] = value; }
-			}
-
-			[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-			public void ValidateIndex(int index)
-			{
-				if (index < 0 || index >= CellCount)
-					throw new ArgumentOutOfRangeException(nameof(index));
-			}
-
-			[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-			public void ValidateCoordinates(IReadOnlyList<int> coordinates)
-			{
-				if (coordinates == null)
-					throw new ArgumentNullException(nameof(coordinates));
-				
-				if (coordinates.Count != NumDimensions)
-					throw new ArgumentException($"coordinates.Count must equal NumDimensions.  Bad count = {coordinates.Count}.", nameof(coordinates));
-
-				for (int i = 0; i < NumDimensions; i++)
-					if (!Extent[i].IsValid(coordinates[i]))
-						throw new ArgumentException($"Coordinate index {i} = {coordinates[i]} is outside the allowed extent of (Min, Max) = ({Extent[i].Min}, {Extent[i].Max}).", nameof(coordinates));
-			}
-
-			[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-			public int ToIndex(IReadOnlyList<int> coordinates)
-			{
-				ValidateCoordinates(coordinates);
-
-				var z = NumDimensions - 1;
-				int index = coordinates[z] - Extent[z].Min;
-				for (int i = z - 1; i >= 0; i--)
+				for (int z = ZMin; z <= ZMax; z++)
 				{
-					index *= Extent[i].Size;
-					index += coordinates[i] - Extent[i].Min;
+					StringBuilder sb = new();
+					for (int y = YMin; y <= YMax; y++)
+					{
+						if (y > YMin) sb.Append('\n');
+						for (int x = XMin; x <= XMax; x++)
+							sb.Append(Contains((x, y, z)) ? '#' : '.');
+					}
+					yield return sb.ToString();
 				}
-
-				return index;
 			}
 
-			public NDimensionalGrid(int numDimensions, IReadOnlyList<InclusiveRange> extent)
-			{
-				if (numDimensions < 1)
-					throw new ArgumentOutOfRangeException(nameof(numDimensions), "numDimensions must be at least 1.");
-
-				if (extent == null)
-					throw new ArgumentNullException(nameof(extent));
-
-				if (extent.Count != numDimensions)
-					throw new ArgumentException("extent.Count must equal numDimensions.", nameof(extent));
-
-				NumDimensions = numDimensions;
-				Extent = extent.ToArray();
-				CellCount = extent.Select(x => x.Size).Aggregate((a,b)=>a*b);
-				grid = new T[CellCount];
-			}
-
-			public NDimensionalGrid(int numDimensions, IReadOnlyList<(int, int)> extent) : this(numDimensions, extent.Select(x => new InclusiveRange(x)).ToList()) {}
+			public int LiveCellCount => live.Count;
 		}
 
-		public void OutputGrid(NDimensionalGrid<bool> grid)
+		void LogPocketDimension(string sectionHeader, PocketDimension pd)
 		{
-			using (Logger.Context("Grid State:"))
-			{
-				var (xe, ye, ze) = (grid.Extent[0], grid.Extent[1], grid.Extent[2]);
-				foreach (var z in ze.YieldInner())
-					using (Logger.Context($"Z = {z}:"))
-						foreach (var y in ye.YieldInner())
-						{	
-							StringBuilder sb = new();
-							foreach (var x in xe.YieldInner())
-								sb.Append(grid[new[]{x,y,z}] ? '#' : '.');
-							Logger.LogLine(sb.ToString());
+			using (Logger.Context(sectionHeader))
+				foreach (var l in pd.ToDisplayStrings().WithIndex())
+					using (Logger.Context($"z = {l.Item2 + pd.ZMin}"))
+						Logger.LogLine(l.Item1);
+		}
+
+		static PocketDimension Iterate(PocketDimension prev)
+		{
+			PocketDimension next = new();
+
+			for (int z = prev.ZMin - 1; z <= prev.ZMax + 1; z++)
+				for (int y = prev.YMin - 1; y <= prev.YMax + 1; y++)
+					for (int x = prev.XMin - 1; x <= prev.XMax + 1; x++)
+					{
+						int liveNeighbors = 0;
+						
+						for (int ox = -1; ox <= 1; ox++)
+							for (int oy = -1; oy <= 1; oy++)
+								for (int oz = -1; oz <= 1; oz++)
+								{
+									if (ox == 0 && oy == 0 && oz == 0)
+										continue;
+
+									if (prev.Contains((x + ox, y + oy, z + oz)))
+										liveNeighbors++;
+								}
+
+						if (prev.Contains((x, y, z)))
+						{
+							if (liveNeighbors == 2 || liveNeighbors == 3)
+								next.Add((x, y, z));
 						}
-			}
-		}
-
-		IEnumerable<int> NeighborOffsets(NDimensionalGrid<bool> grid)
-		{
-			var originIndex = grid.ToIndex(new[]{0,0,0});
-			for (int x = -1; x <= 1; x++)
-				for (int y = -1; y <= 1; y++)
-					for (int z = -1; z <= 1; z++)
-					{
-						if (x == 0 && y == 0 && z == 0)
-							continue;
-						yield return grid.ToIndex(new[]{x,y,z})-originIndex;
-					}
-		}
-
-		NDimensionalGrid<bool> Iterate(NDimensionalGrid<bool> grid)
-		{
-			var old = grid;
-			grid = new NDimensionalGrid<bool>(old.NumDimensions, old.Extent.Select(x => new InclusiveRange((x.Min - 1, x.Max + 1))).ToList());
-			var sourceOffsets = NeighborOffsets(old).ToArray();
-
-			foreach (var z in old.Extent[2].YieldInner())
-				foreach (var y in old.Extent[1].YieldInner())
-					foreach (var x in old.Extent[0].YieldInner())
-					{
-						var currentCoordinates = new[]{x,y,z};
-						var currentSourceIndex = old.ToIndex(currentCoordinates);
-						int active = 0;
-						for (int i = 0; i < 26; i++)
-							if (old[currentSourceIndex + sourceOffsets[i]])
-								active++;
-						if (old[currentSourceIndex])
-							grid[currentCoordinates] = active == 2 || active == 3;
 						else
-							grid[currentCoordinates] = active == 3;
+						{
+							if (liveNeighbors == 3)
+								next.Add((x, y, z));
+						}
 					}
 
-			return grid;
+			return next;
 		}
 
-		public override object Part1(string rawInput)
+
+		public override object Part1(string input)
 		{
-			var lines = rawInput.ToLines().ToList();
-			var initialHeight = lines.Count;
-			var initialWidth = lines[0].Length;
+			PocketDimension pd = new();
+			foreach (var (line, y) in input.ToLines().WithIndex())
+				foreach (var (v, x) in line.WithIndex())
+					if (v == '#')
+						pd.Add((x, y, 0));
 
-			var grid = new NDimensionalGrid<bool>(3, new[]{(-1,initialWidth),(-1,initialHeight),(-2,2)} );
+			LogPocketDimension("Initial State:", pd);
 
-			var z = 0;
-			for (int y = 0; y < initialHeight; y++)
+			foreach (var cycle in Enumerable.Range(1, 6))
 			{
-				var line = lines[y];
-				for (int x = 0; x < initialWidth; x++)
-					grid[new[]{x,y,z}] = line[x] == '#';
-			}
-			
-			using (Logger.Context("\nInitial State:"))
-				OutputGrid(grid);
-
-			foreach (var n in Enumerable.Range(1, 6))
-			{
-				grid = Iterate(grid);
-				using (Logger.Context($"\nAfter {n} cycles:"))
-					OutputGrid(grid);
+				pd = Iterate(pd);
+				LogPocketDimension($"\nAfter {cycle} cycles:", pd);
 			}
 
-			return -1;
+			return pd.LiveCellCount;
+		}
+
+		public class PocketDimension2
+		{
+			private readonly HashSet<(int, int, int, int)> live = new();
+
+			public int XMin { get; private set; }
+			public int XMax { get; private set; }
+			public int YMin { get; private set; }
+			public int YMax { get; private set; }
+			public int ZMin { get; private set; }
+			public int ZMax { get; private set; }
+			public int QMin { get; private set; }
+			public int QMax { get; private set; }
+
+			public void Add((int, int, int, int) c)
+			{
+				if (c.Item1 < XMin) XMin = c.Item1;
+				if (c.Item1 > XMax) XMax = c.Item1;
+
+				if (c.Item2 < YMin) YMin = c.Item2;
+				if (c.Item2 > YMax) YMax = c.Item2;
+
+				if (c.Item3 < ZMin) ZMin = c.Item3;
+				if (c.Item3 > ZMax) ZMax = c.Item3;
+
+				if (c.Item4 < QMin) QMin = c.Item4;
+				if (c.Item4 > QMax) QMax = c.Item4;
+
+				live.Add(c);
+			}
+
+			public bool Contains((int, int, int, int) c)
+			{
+				return live.Contains(c);
+			}
+
+			public int LiveCellCount => live.Count;
+		}
+
+		static PocketDimension2 Iterate2(PocketDimension2 prev)
+		{
+			PocketDimension2 next = new();
+
+			for (int q = prev.QMin - 1; q <= prev.QMax + 1; q++)
+				for (int z = prev.ZMin - 1; z <= prev.ZMax + 1; z++)
+					for (int y = prev.YMin - 1; y <= prev.YMax + 1; y++)
+						for (int x = prev.XMin - 1; x <= prev.XMax + 1; x++)
+						{
+							int liveNeighbors = 0;
+						
+							for (int ox = -1; ox <= 1; ox++)
+								for (int oy = -1; oy <= 1; oy++)
+									for (int oz = -1; oz <= 1; oz++)
+										for (int oq = -1; oq <= 1; oq++)
+										{
+											if (ox == 0 && oy == 0 && oz == 0 && oq == 0)
+												continue;
+
+											if (prev.Contains((x + ox, y + oy, z + oz, q + oq)))
+												liveNeighbors++;
+										}
+
+							if (prev.Contains((x, y, z, q)))
+							{
+								if (liveNeighbors == 2 || liveNeighbors == 3)
+									next.Add((x, y, z, q));
+							}
+							else
+							{
+								if (liveNeighbors == 3)
+									next.Add((x, y, z, q));
+							}
+						}
+
+			return next;
+		}
+
+
+		public override object Part2(string input)
+		{
+			PocketDimension2 pd = new();
+			foreach (var (line, y) in input.ToLines().WithIndex())
+				foreach (var (v, x) in line.WithIndex())
+					if (v == '#')
+						pd.Add((x, y, 0, 0));
+
+			foreach (var cycle in Enumerable.Range(1, 6))
+				pd = Iterate2(pd);
+
+			return pd.LiveCellCount;
 		}
 	}
 }
