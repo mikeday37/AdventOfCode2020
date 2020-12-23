a63894ae06c3178df2601cc7282d565c6baa1494
mikeday37 <mikeday370146@gmail.com>
2020-12-23 17:22:08 -0500

    day 23 part 2

--- AdventOfCode2020/Challenges/Day23/Day23.cs
+++ AdventOfCode2020/Challenges/Day23/Day23.cs
@@ -53,26 +53,63 @@ namespace AdventOfCode2020.Challenges.Day23
 
 		public override object Part1(string input)
 		{
-			// parse the labels into cups and setup a tracking dictionary, marking all as initially available
-			Cup top, cur;
-			top = cur = ParseCupsAndReturnTop(input);
+			var (cup1, lowest, highest) = PlayCrabCups(ParseCupsAndReturnTop(input), 100, true);
+
+			// calculate result string
+			var result = new string(FollowCupsOnce(cup1.Next).Take(highest - lowest).Select(x => $"{x.Label}"[0]).ToArray());
+			Logger.LogLine($"Result: {result}");
+
+			return result;
+		}
+
+		public override object Part2(string input)
+		{
+			var top = ParseCupsAndReturnTop(input);
+			Logger.LogLine("Extending...");
+			ExtendToOneMillionCups(top);
+			var a = FollowCupsOnce(PlayCrabCups(top, 10000000, false).cup1.Next).Take(2).ToArray();
+			return (long)a[0].Label * (long)a[1].Label;
+		}
+
+		private void ExtendToOneMillionCups(Cup top)
+		{
+			var n = FollowCupsOnce(top).Select(x => x.Label).Max();
+			Cup prev = FollowCupsOnce(top).Last(), cur = null;
+			while (++n <= 1000000)
+			{
+				cur = new Cup{Label = n};
+				prev.Next = cur;
+				prev = cur;
+			}
+			cur.Next = top;
+		}
+
+		public (Cup cup1, int lowest, int highest) PlayCrabCups(Cup top, int moves, bool verboseLogging)
+		{
+			Logger.LogLine("Tracking...");
 			Dictionary<int, (bool available, Cup cup)> track = FollowCupsOnce(top)
 				.OrderBy(x => x.Label).ToDictionary(x => x.Label, x => (true, x));
 			var (lowest, highest) = (track.Keys.Min(), track.Keys.Max());
+			Logger.LogLine("Moving...");
 
 			// do X moves
-			const int moves = 100;
+			var cur = top;
 			foreach (var move in Enumerable.Range(1, moves))
 			{
 				// log move state
-				Logger.LogLine($"\n-- move {move} --");
-				Logger.LogLine($"cups: {string.Join(" ", FollowCupsOnce(top).Select(x => x == cur ? $"({x.Label})" : $"{x.Label}"))}");
+				if (verboseLogging)
+				{
+					Logger.LogLine($"\n-- move {move} --");
+					Logger.LogLine($"cups: {string.Join(" ", FollowCupsOnce(top).Select(x => x == cur ? $"({x.Label})" : $"{x.Label}"))}");
+				}
+				else if (0 == move % 10000)
+					Logger.LogLine($"on move {move}...");
 
 				// pick up 3 cups after cur
 				var (a, b) = (cur.Next, FollowCupsOnce(cur.Next).Take(3).Last());
 				cur.Next = b.Next;
 				b.Next = null;
-				Logger.LogLine($"pick up: {string.Join(", ", FollowCupsOnce(a).Select(x => x.Label))}");
+				if (verboseLogging) Logger.LogLine($"pick up: {string.Join(", ", FollowCupsOnce(a).Select(x => x.Label))}");
 
 				// mark picks as unavailable
 				foreach (var picked in FollowCupsOnce(a))
@@ -86,7 +123,7 @@ namespace AdventOfCode2020.Challenges.Day23
 						dest = highest;
 				}
 				while (!track[dest].available);
-				Logger.LogLine($"destination: {dest}");
+				if (verboseLogging) Logger.LogLine($"destination: {dest}");
 
 				// mark picks as available again
 				foreach (var picked in FollowCupsOnce(a))
@@ -102,19 +139,14 @@ namespace AdventOfCode2020.Challenges.Day23
 			}
 
 			// log final positions
-			Logger.LogLine($"\n-- final --");
-			Logger.LogLine($"cups: {string.Join(" ", FollowCupsOnce(top).Select(x => x == cur ? $"({x.Label})" : $"{x.Label}"))}");
+			if (verboseLogging)
+			{
+				Logger.LogLine($"\n-- final --");
+				Logger.LogLine($"cups: {string.Join(" ", FollowCupsOnce(top).Select(x => x == cur ? $"({x.Label})" : $"{x.Label}"))}");
+			}
 
-			// calculate result string
-			var result = new string(FollowCupsOnce(track[1].cup.Next).Take(highest - lowest).Select(x => $"{x.Label}"[0]).ToArray());
-			Logger.LogLine($"Result: {result}");
-
-			return result;
-		}
-
-		public override object Part2(string input)
-		{
-			return -1;
+			// return cup 1, lowest, and highest
+			return (track[1].cup, lowest, highest);
 		}
 	}
 }
