1303acc0e7a7f77a928c6b8b81fecdea55833dd2
mikeday37 <mikeday370146@gmail.com>
2020-12-28 13:25:27 -0500

    increasing clarity of the day 19 code so far

--- AdventOfCode2020/Challenges/Day19/Day19NoRegex.cs
+++ AdventOfCode2020/Challenges/Day19/Day19NoRegex.cs
@@ -148,21 +148,143 @@ namespace AdventOfCode2020.Challenges.Day19
 
 			public MessageTester(IReadOnlyDictionary<int, Rule> rules)
 			{
-				rootLink = new(){Rule = rules[0]};
-				unresolvedInitialLeafLinks.Add(rootLink);
+				rootLink = GrowInitialTree(rules);
+				FindAllCycles();
+				//DetermineBounds();
+				LogTree("Tree Results:");
+			}
 
+			private readonly Link rootLink;
+
+			/// <summary>
+			/// Takes the dictionary of rules and implements them as a "tree" of Node objects connected by Link objects,
+			/// setting the PreExisting flag on all but the "first" Link to any given rule.  Returns the rootLink of the tree.
+			/// </summary>
+			/// <remarks>
+			/// The resulting "tree" is only a strict tree in the sense that you can treat it as a tree if you don't follow links where
+			/// PreExisting == true.  If you ignore that flag, the result is actually a directed graph, which may contain any number
+			/// of cycles (infinite loops).  The logic and types used by this method safely handle all such loops in a reliable way,
+			/// and sets us up to efficiently identify all cycles (if any) for later analysis.
+			/// </remarks>
+			private static Link GrowInitialTree(IReadOnlyDictionary<int, Rule> rules)
+			{
+				// the root is always for rule 0
+				Link rootLink = new(){Rule = rules[0]};
+
+				// we'll need to keep track of what node implements what rule, in order to set the PreExisting flags correctly.
+				Dictionary<int, Node> ruleNodes = new();
+				
+				// start growing the tree from its root
+				HashSet<Link> unresolvedLinks = new();
+				unresolvedLinks.Add(rootLink);
+
+				// keep growing until there are no more links left unresolved
 				using (ThreadLogger.Context("Growing Initial Tree..."))
-					while (unresolvedInitialLeafLinks.Any())
-						foreach (var link in unresolvedInitialLeafLinks.ToList())
-							GrowInitialTree(link, rules);
+					while (unresolvedLinks.Any())
+						foreach (var link in unresolvedLinks.ToList()) // .ToList() because we change the hashset we're iterating over
+							GrowInitialTree(link);
 
+				// return the root now that it's fully grown
+				return rootLink;
+
+				// helper method to grow the tree one unresolved link at a time:
+				//
+				// take the given unresolvedLink (AL), remove it from the set, and resolving it by
+				// creating the Node(s) and Link(s) (if any) responsible for directly implementing its Rule,
+				// and add to the set a new unresolvedLink for each sub-rule any of those new nodes must reference.
+				void GrowInitialTree(Link AL)
+				{
+					// remove from the set, since it will be resolved when we're done.
+					unresolvedLinks.Remove(AL);
+
+					// see if a node already exists for the rule of this unresolved link
+					bool preExisting = ruleNodes.TryGetValue(AL.Rule.Index, out var A);
+					if (preExisting)
+					{
+						// if so, just mark the link as pointing to a "PreExisting" node
+						AL.PreExisting = true;
+
+						// and set it's child to the node we already found.
+						AL.Child = A;
+
+						// note: could mark A's "additional parent links" at this point if desired
+					}
+					else
+					{
+						// otherwise, we must actually implement the rule, depending on its type
+						switch (AL.Rule.Type)
+						{
+							// A Character rule is directly implemented by a single node (A) with no further references.
+							//
+							// AL -> A
+							//
+							case RuleType.Character: 
+								A = new CharacterNode{Content = AL.Rule.Character};
+								break;
+
+							// A Sequence rule is implemented by a node for the sequence itself (A), having as children
+							// a new (initially unresolved) Link (BL) for each rule it references.
+							//
+							// AL -> A ->> BL
+							//
+							case RuleType.Sequence: 
+								A = new SequenceNode();
+								foreach (var b in AL.Rule.Sequence)
+								{
+									var BL = new Link{Parent = A, Rule = rules[b]};
+									A.ChildLinks.Add(BL);
+									unresolvedLinks.Add(BL);
+								}
+								break;
+							
+							// An AlternativeSequence is implemented as a sub-tree exactly 
+							// like a Sequence of Sequences, except that the root Node (A) of this sub-tree
+							// is actually an AlternativeNode instead of a SequenceNode.
+							//
+							// AL -> A ->> BL -> B ->> CL
+							//
+							case RuleType.AlternativeSequences:
+								A = new AlternativeNode();
+								foreach (var b in AL.Rule.AlternativeSequences)
+								{
+									var B = new SequenceNode();
+									var BL = new Link{Parent = A, Child = B};
+									A.ChildLinks.Add(BL);
+									B.ParentLink = BL;
+									foreach (var c in b)
+									{
+										var CL = new Link{Parent = B, Rule = rules[c]};
+										B.ChildLinks.Add(CL);
+										unresolvedLinks.Add(CL);
+									}
+								}
+								break;
+						}
+
+						// connect the link to the new root of the subtree we just created
+						AL.Child = A;
+						A.ParentLink = AL;
+
+						// record the node as implemented for the rule
+						ruleNodes[AL.Rule.Index] = A;
+					}
+				}
+			}
+
+			/// <summary>
+			/// Efficiently finds and marks all links that directly cause a cycle (infinite loop), and all links that
+			/// reference them directly or not, which are thus implemented by cycles.  This is important for reliably
+			/// determining which nodes must support backtracking.
+			/// </summary>
+			private void FindAllCycles()
+			{
 				using (ThreadLogger.Context("Finding Cycles..."))
 				{
 					HashSet<Node> stack = new();
 					HashSet<Link> potential = new(), cyclic = new();
 					int directCycles = 0;
 
-					void DepthFirstSearchForCycles(Link link)
+					void RecursivelySearchForCycles(Link link)
 					{
 						var node = link.Child;
 						potential.Add(link);
@@ -180,7 +302,7 @@ namespace AdventOfCode2020.Challenges.Day19
 								potential.Clear();
 							}
 							else
-								DepthFirstSearchForCycles(childLink);
+								RecursivelySearchForCycles(childLink);
 						}
 
 						stack.Remove(node);
@@ -188,7 +310,7 @@ namespace AdventOfCode2020.Challenges.Day19
 					}
 
 					using (ThreadLogger.Context("Finding Direct Cycles..."))
-						DepthFirstSearchForCycles(rootLink);
+						RecursivelySearchForCycles(rootLink);
 
 					using (ThreadLogger.Context("Marking Indirect Cycles..."))
 						foreach (var l in cyclic)
@@ -199,7 +321,18 @@ namespace AdventOfCode2020.Challenges.Day19
 
 					ThreadLogger.LogLine($"Cycle Count: Direct = {directCycles}, Indirect = {cyclic.Count}, Total = {cyclic.Count + directCycles}");
 				}
+			}
 
+			/// <summary>
+			/// Recursively logs the tree, safely avoiding any infinite loops.
+			/// The "PreExisting" flag is used to avoid repeatedly logging the
+			/// details of rules that have already been logged.
+			/// 
+			/// The result is that every rule that is actually referenced is
+			/// fully logged.
+			/// </summary>
+			private void LogTree(string message)
+			{
 				static void LogTree(Link link)
 				{
 					using (ThreadLogger.Context(link.ToString()))
@@ -211,63 +344,8 @@ namespace AdventOfCode2020.Challenges.Day19
 									LogTree(childLink);
 				}
 
-				using (ThreadLogger.Context("Tree Results:"))
+				using (ThreadLogger.Context(message))
 					LogTree(rootLink);
-			}
-
-			private readonly Link rootLink;
-			private readonly Dictionary<int, Node> ruleNodes = new();
-			private readonly HashSet<Link> unresolvedInitialLeafLinks = new();
-
-			private void GrowInitialTree(Link AL, IReadOnlyDictionary<int, Rule> rules)
-			{
-				unresolvedInitialLeafLinks.Remove(AL);
-				bool preExisting = ruleNodes.TryGetValue(AL.Rule.Index, out var A);
-				if (preExisting)
-				{
-					AL.PreExisting = true;
-					AL.Child = A;
-					// note: could mark A's "additional parent links" at this point if desired
-				}
-				else
-				{
-					switch (AL.Rule.Type)
-					{
-						case RuleType.Character: // AL -> A
-							A = new StringNode{Content = new string(AL.Rule.Character, 1)};
-							break;
-
-						case RuleType.Sequence: // AL -> A ->> BL
-							A = new SequenceNode();
-							foreach (var b in AL.Rule.Sequence)
-							{
-								var BL = new Link{Parent = A, Rule = rules[b]};
-								A.ChildLinks.Add(BL);
-								unresolvedInitialLeafLinks.Add(BL);
-							}
-							break;
-							
-						case RuleType.AlternativeSequences: // AL -> A ->> BL -> B ->> CL
-							A = new AlternativeNode();
-							foreach (var b in AL.Rule.AlternativeSequences)
-							{
-								var B = new SequenceNode();
-								var BL = new Link{Parent = A, Child = B};
-								A.ChildLinks.Add(BL);
-								B.ParentLink = BL;
-								foreach (var c in b)
-								{
-									var CL = new Link{Parent = B, Rule = rules[c]};
-									B.ChildLinks.Add(CL);
-									unresolvedInitialLeafLinks.Add(CL);
-								}
-							}
-							break;
-					}
-					AL.Child = A;
-					A.ParentLink = AL;
-					ruleNodes[AL.Rule.Index] = A;
-				}
 			}
 
 			private class Link
@@ -316,6 +394,7 @@ namespace AdventOfCode2020.Challenges.Day19
 				public List<Link> ChildLinks {get;}
 				public Node() => ChildLinks = new();
 				public virtual bool Stateful => false;
+				public virtual bool MayRequireBacktracking => false;
 
 				public override string ToString()
 				{
@@ -330,9 +409,9 @@ namespace AdventOfCode2020.Challenges.Day19
 				public virtual TState NewState() => new TState();
 			}
 
-			private class StringNode : Node
+			private class CharacterNode : Node
 			{
-				public string Content {get; set;}
+				public char Content {get; set;}
 
 				public override string ToString()
 				{
@@ -344,6 +423,7 @@ namespace AdventOfCode2020.Challenges.Day19
 			{
 				public class State
 				{
+					public int NextSequence = 0;
 				}
 			}
 
@@ -351,7 +431,10 @@ namespace AdventOfCode2020.Challenges.Day19
 			{
 				public class State
 				{
+					public int NextAlternative = 0;
 				}
+
+				public override bool MayRequireBacktracking => true;
 			}
 		}
 	}
